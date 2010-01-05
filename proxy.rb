126dbdc4dc581ad76d14332b04e77c816a2ee888
Tim Parkin <tparkin@gmail.com>
2010-01-05 10:26:07 +0800

    added basic auth and yml config file for hosts and credentials

--- proxy.rb
+++ proxy.rb
@@ -7,6 +7,10 @@ require 'net/http'
 # A simple web proxy that supports GET/POST
 # with basic authentication
 class SimpleProxy < Mongrel::HttpHandler
+  def initialize
+    @credentials = YAML::load(File.open('config.yml'))
+  end
+
   def process(request, response)
     params = request.params
     res = fetch(params['REQUEST_URI'])
@@ -25,8 +29,25 @@ class SimpleProxy < Mongrel::HttpHandler
     case response
       when Net::HTTPSuccess then response
       when Net::HTTPRedirection then fetch(response['location'], limit - 1)
+      when Net::HTTPUnauthorized then fetch_authorized(uri)
     else
       response.error!
+    end
+  end
+
+  def fetch_authorized(uri)
+    url = URI.parse(uri)
+    login = @credentials[url.host]
+
+    unless login.nil?
+    # This will change later, when posts are supported
+      req = Net::HTTP::Get.new(url.path, {"User-Agent" => "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US) AppleWebKit/525.13 (KHTML, like Gecko) Chrome/0.A.B.C Safari/525.13"})
+      req.basic_auth login['user'], login['pass']
+      response = Net::HTTP.start(url.host, url.port) { |http|
+        http.request(req)
+      }
+
+      # Technically this should case response for errors
     end
   end
 end
